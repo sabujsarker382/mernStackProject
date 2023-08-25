5e813915985d3c83a76a0fd9a676b9e85eba112d
sabujsarker382~ <raj16@cse.pstu.ac.bd>
2023-08-25 18:54:10 +0600

    Server Master

---
+++ routes/auth.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const router = express.Router();
+
+// middlewares
+const { requireSignin, isAdmin } = require("../middlewares/auth.js");
+
+
+// controllers
+const { register, login, secret, updateProfile, getOrders,
+    allOrders, } = require("../controllers/auth.js");
+
+router.post("/register", register);
+router.post("/login", login);
+
+router.get("/auth-check", requireSignin, (req, res) => {
+    res.json({ ok: true });
+});
+router.get("/admin-check", requireSignin, isAdmin, (req, res) => {
+    res.json({ ok: true });
+});
+
+// testing
+router.get("/secret", requireSignin, isAdmin, secret);
+
+router.put("/profile", requireSignin, updateProfile);
+
+// orders
+router.get("/orders", requireSignin, getOrders);
+router.get("/all-orders", requireSignin, isAdmin, allOrders);
+
+module.exports = router;
